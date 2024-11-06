e69d9b4ff05da3fdaf25ac012a991f1a8dd27dab
soundarya-poddaturi <soundarya2873@gmail.com>
2024-11-06 10:28:19 +0530

    9th

--- frontend/src/components/PageNotFound.js
+++ frontend/src/components/PageNotFound.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import img from '../images/notfound.jpg'
 
 export const PageNotFound = () => {
   return (
     <>
-    <div>PageNotFound</div>
-    <h1> page not found</h1>
+      
+      <div
+        className='pg d-flex justify-content-center'
+        style={{
+          width: '100%', // Sets the width of the div
+          height: '80vh', // Adjust this height as needed to display the image fully
+          backgroundImage: `url(${img})`,
+          backgroundSize: 'contain', // Change to 'contain' to see the full image
+          backgroundRepeat: 'no-repeat', // Prevents the image from repeating
+          backgroundPosition: 'center',
+        }}
+      ></div>
     </>
   )
 }
